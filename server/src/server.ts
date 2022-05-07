abdea2ff93b702d3a6d7397bf7d1a19b7f8ed3c2
Shayane <shayaneteixeira56@gmail.com>
2022-05-07 14:37:53 -0300

    changes (deploy)

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,15 +5,12 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server running!');
 });
-
 
 // GET, POST, PUT, PATCH, DELETE
 
